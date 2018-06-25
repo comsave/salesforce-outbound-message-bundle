dae8e7ca05b2b24167f7943f6f2e9be48f06031f
Dirk Scholten <dirk.scholten@comsave.com>
2018-06-25 13:05:28 +0200

    Registered the extension class

--- src/SalesforceOutboundMessageBundle/SalesforceOutboundMessageBundle.php
+++ src/SalesforceOutboundMessageBundle/SalesforceOutboundMessageBundle.php
@@ -2,9 +2,13 @@
 
 namespace SalesforceOutboundMessageBundle;
 
+use SalesforceOutboundMessageBundle\DependencyInjection\SalesforceOutboundMessageExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class SalesforceOutboundMessageBundle extends Bundle
 {
-
+    public function getContainerExtension()
+    {
+        return new SalesforceOutboundMessageExtension();
+    }
 }
