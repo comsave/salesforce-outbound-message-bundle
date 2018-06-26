17329076dc66014c4b5df0ce358696690822d849
Dirk Scholten <dirk.scholten@comsave.com>
2018-06-26 14:46:46 +0200

    Added service to services.yml

--- src/Comsave/SalesforceOutboundMessageBundle/Services/RequestHandler/OutboundMessageRequestHandler.php
+++ src/Comsave/SalesforceOutboundMessageBundle/Services/RequestHandler/OutboundMessageRequestHandler.php
@@ -25,22 +25,22 @@ class OutboundMessageRequestHandler
      */
     private $outboundMessageObjectNameRetriever;
 
-//    /**
-//     * OutboundMessageRequestHandler constructor.
-//     * @param OutboundMessageSoapServerBuilder $outboundMessageSoapServerBuilder
-//     * @param SoapResponseBuilder $soapServerResponseBuilder
-//     * @param OutboundMessageObjectNameRetriever $outboundMessageObjectNameRetriever
-//     * @codeCoverageIgnore
-//     */
-//    public function __construct(
-//        OutboundMessageSoapServerBuilder $outboundMessageSoapServerBuilder,
-//        SoapResponseBuilder $soapServerResponseBuilder,
-//        OutboundMessageObjectNameRetriever $outboundMessageObjectNameRetriever)
-//    {
-//        $this->outboundMessageSoapServerBuilder = $outboundMessageSoapServerBuilder;
-//        $this->soapServerResponseBuilder = $soapServerResponseBuilder;
-//        $this->outboundMessageObjectNameRetriever = $outboundMessageObjectNameRetriever;
-//    }
+    /**
+     * OutboundMessageRequestHandler constructor.
+     * @param OutboundMessageSoapServerBuilder $outboundMessageSoapServerBuilder
+     * @param SoapResponseBuilder $soapServerResponseBuilder
+     * @param OutboundMessageObjectNameRetriever $outboundMessageObjectNameRetriever
+     * @codeCoverageIgnore
+     */
+    public function __construct(
+        OutboundMessageSoapServerBuilder $outboundMessageSoapServerBuilder,
+        SoapResponseBuilder $soapServerResponseBuilder,
+        OutboundMessageObjectNameRetriever $outboundMessageObjectNameRetriever)
+    {
+        $this->outboundMessageSoapServerBuilder = $outboundMessageSoapServerBuilder;
+        $this->soapServerResponseBuilder = $soapServerResponseBuilder;
+        $this->outboundMessageObjectNameRetriever = $outboundMessageObjectNameRetriever;
+    }
 
     /**
      * @param string $xml
@@ -49,13 +49,12 @@ class OutboundMessageRequestHandler
      */
     public function handle(string $xml): Response
     {
-//        $soapServer = $this->outboundMessageSoapServerBuilder->build($this->outboundMessageObjectNameRetriever->retrieve($xml));
-//        ob_start();
-//        $soapServer->handle($xml);
-//        $responseContent = ob_get_contents();
-//        ob_end_clean();
-//
-//        return $this->soapServerResponseBuilder->build($responseContent);
-        return 'success';
+        $soapServer = $this->outboundMessageSoapServerBuilder->build($this->outboundMessageObjectNameRetriever->retrieve($xml));
+        ob_start();
+        $soapServer->handle($xml);
+        $responseContent = ob_get_contents();
+        ob_end_clean();
+
+        return $this->soapServerResponseBuilder->build($responseContent);
     }
 }
